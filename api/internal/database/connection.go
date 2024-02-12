932174a5c8d9fd83c35e0fd256d3babc32b72fb6
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2024-02-12 14:58:07 -0300

    Database connection

---
+++ api/internal/database/connection.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var dbConn *sql.DB
+
+func Init() {
+
+	host := os.Getenv("DB_HOST")
+
+	if host == "" {
+		host = "localhost"
+	}
+
+	config := fmt.Sprintf("host=%s port=5432 user=admin password=admin123 dbname=financial sslmode=disable", host)
+
+	db, err := sql.Open("postgres", config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	dbConn = db
+}
+
+func GetConnection() *sql.DB {
+	return dbConn
+}
