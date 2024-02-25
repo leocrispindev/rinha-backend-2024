3621b42ff61c8dc9399638915dae63cf0ddbfe8d
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2024-02-25 00:41:46 -0300

    fix endpoints

--- api/cmd/api.go
+++ api/cmd/api.go
@@ -11,7 +11,7 @@ func main() {
 	database.Init()
 
 	r := gin.Default()
-	r.POST("/clientes/:id/transacao", controller.HandlerTransaction)
+	r.POST("/clientes/:id/transacoes", controller.HandlerTransaction)
 	r.GET("/clientes/:id/extrato", controller.HandlerExtract)
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":9999") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
